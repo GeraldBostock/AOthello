ec8840474df941225fc0a0ee2f3b6c6bc6061a77
Doğa Uzunali <doga.uzunali@gmail.com>
2019-04-22 22:32:25 +0300

    added screenshot, removed redundant code

--- src/ai_player.py
+++
@@ -1,6 +0,0 @@
-from player import IPlayer
-
-class AIPlayer(IPlayer):
-    def get_move(self, board):
-        if board.is_move_viable((3, 2)):
-            return (3, 2)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from game import Game
 from human_player import HumanPlayer
-from ai_player import AIPlayer
 
 def main():
         game = Game(600, 600)
         game.set_black_player(HumanPlayer())
-        game.set_white_player(AIPlayer())
+        game.set_white_player(HumanPlayer())
         game.run()
 
 if __name__ == "__main__":
